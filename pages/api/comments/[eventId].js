8b9a0fa12de1176726c742c8b728c822fd08f595
faysal725 <afaysal725@gmail.com>
2024-03-06 21:09:03 +0600

    mongodb finished

--- pages/api/comments/[eventId].js
+++ pages/api/comments/[eventId].js
@@ -1,39 +1,63 @@
-function handler(req, res) {
-    const eventId = req.query.eventId 
+import { connectDatabase, getAllDocuments, insertDocument } from "@/helpers/db-utill";
+import { MongoClient } from "mongodb";
 
-    if (req.method === 'POST') {
-        // add server side validation 
-        
-        const { email, name, text} = req.body;
-        if (
-            !email.includes('@') || 
-            !name || 
-            !name.trim() === '' ||
-            !text ||
-            text.trim() === ''
-        ) {
-            res.status(422).json({message: 'Invalid input'})
-            return ;
-        }
-        const newComment = {
-            id: new Date().toISOString(),
-            email,
-            name,
-            text 
-        }
-        console.log(newComment)
-        res.status(201).json({message: 'added comment.', comment: newComment})
+async function handler(req, res) {
+  const eventId = req.query.eventId;
+
+
+  let client;
+  try {
+    client = await connectDatabase();
+  } catch (error) {
+    res.status(500).json({ message: "Connection to the database failed" });
+    return;
+  }
+
+  if (req.method === "POST") {
+    // add server side validation
+
+    const { email, name, text } = req.body;
+    if (
+      !email.includes("@") ||
+      !name ||
+      !name.trim() === "" ||
+      !text ||
+      text.trim() === ""
+    ) {
+      res.status(422).json({ message: "Invalid input" });
+      client.close();
+      return;
     }
+    const newComment = {
+      email,
+      name,
+      text,
+      eventId,
+    };
+    let result; 
+    try {
+        result = await insertDocument(client, "comments", newComment);
+        newComment._id = result.insertedId;
+        res.status(201).json({ message: "added comment.", comment: newComment });
 
-    if (req.method === 'GET') {
-        const dummyList = [
-            {id: 'c1', name: 'Max', text: 'A fist comment!'},
-            {id: 'c2', name: 'Manuel', text: 'A second comment!'},
-
-        ]
-
-        res.status(200).json({comments: dummyList})
+    } catch (error) {
+        res.status(500).json({message: 'Inserting comment failed!'})
     }
+    // const result = await insertDocument(client, "comments", newComment);
+
+  }
+
+  if (req.method === "GET") {
+    try {
+        const documents = await getAllDocuments(client, 'comments', {_id: -1})
+        res.status(200).json({ comments: documents });
+    } catch (error) {
+        res.status(500).json({ message: 'getting comments failed' });
+         
+    }
+  }
+
+  client.close();
 }
 
 export default handler;
--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,4 +1,19 @@
+import { connectDatabase, insertDocument } from "@/helpers/db-utill";
 import { MongoClient } from "mongodb";
+
+// async function connectDatabase() {
+//   const client = await MongoClient.connect(
+//     "mongodb+srv://next-course:Ff123456789@cluster0.hozny.mongodb.net/events?retryWrites=true&w=majority&appName=Cluster0"
+//   );
+//   return client;
+// }
+
+// async function insertDocument(client, document) {
+//   const db = client.db();
+
+//   await db.collection("newsletter").insertOne(document);
+// }
+
 
 async function handler(req, res) {
   if (req.method == "POST") {
@@ -7,13 +22,23 @@ async function handler(req, res) {
       res.status(422).json({ message: "Invalid emial address." });
       return;
     }
-    const client = await MongoClient.connect(
-      "mongodb+srv://next-course:Ff123456789@cluster0.hozny.mongodb.net/newsletter?retryWrites=true&w=majority&appName=Cluster0"
-    );
-    const db = client.db();
 
-    await db.collection("emails").insertOne({ email: userEmail });
-    client.close();
+    let client;
+    try {
+      client = await connectDatabase();
+    } catch (error) {
+        res.status(500).json({message: 'connecting to the database failed'})
+        return;
+    }
+
+    try {
+      await insertDocument(client, 'newsletter', { email: userEmail });
+      client.close();
+    } catch (error) {
+        res.status(500).json({message: 'inserting data failed'})
+        return;
+    }
+
     console.log(userEmail);
     res.status(201).json({ message: "Signed Up" });
   }
